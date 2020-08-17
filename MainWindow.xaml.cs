f4fc0a527f0226338bf77ae0d20818d4c9987e23
Peter Gustafsson <the.peter.gustafsson@gmail.com>
2020-08-17 21:21:15 +0200

    Now using PDFSharp

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.IO;
 using System.Windows;
 using Microsoft.Win32;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Pdf.Security;
 
 namespace ZarasPDFPasswordProtector
 {
@@ -18,9 +21,39 @@ namespace ZarasPDFPasswordProtector
 
         private void SetPassword(object sender, RoutedEventArgs e)
         {
-
-            MessageBox.Show(PasswordBox.Text);
+            SetPasswordOnFile(PasswordBox.Text);
+            MessageBox.Show("A password protected copy was created");
         }
+
+        private void SetPasswordOnFile(string password)
+        {
+            // Open an existing document. Providing an unrequired password is ignored.
+            PdfDocument document = PdfReader.Open(_originalFile.FullName);
+
+            PdfSecuritySettings securitySettings = document.SecuritySettings;
+
+            // Setting one of the passwords automatically sets the security level to 
+            // PdfDocumentSecurityLevel.Encrypted128Bit.
+            securitySettings.UserPassword = password;
+            //securitySettings.OwnerPassword = "owner";
+
+            // Don't use 40 bit encryption unless needed for compatibility reasons
+            //securitySettings.DocumentSecurityLevel = PdfDocumentSecurityLevel.Encrypted40Bit;
+
+            // Restrict some rights.
+            securitySettings.PermitAccessibilityExtractContent = false;
+            securitySettings.PermitAnnotations = false;
+            securitySettings.PermitAssembleDocument = false;
+            securitySettings.PermitExtractContent = false;
+            securitySettings.PermitFormsFill = true;
+            securitySettings.PermitFullQualityPrint = false;
+            securitySettings.PermitModifyDocument = true;
+            securitySettings.PermitPrint = false;
+
+            // Save the document...
+            document.Save(_originalFile.FullName.Replace(".pdf", "_protected.pdf"));
+        }
+
 
         private void SelectFile(object sender, RoutedEventArgs e)
         {
