6dea1285d9878337a2ab5ed621bd8df468b69c60
Peter Gustafsson <the.peter.gustafsson@gmail.com>
2020-08-17 20:25:39 +0200

    Using .NET Framework 4.6 instead to support old clients.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace ZarasPDFPasswordProtector
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FileInfo _originalFile;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SetPassword(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show(PasswordBox.Text);
+        }
+
+        private void SelectFile(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "PDF files (*.pdf)|*.pdf|All files (*.*)|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // do something with the filename
+                //MessageBox.Show(openFileDialog.FileName);
+
+                _originalFile = new FileInfo(openFileDialog.FileName);
+
+                OriginalFilePath_Text.Text = _originalFile.Name;
+            }
+        }
+    }
+}
